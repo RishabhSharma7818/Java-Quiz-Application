86c14ccc2653bbdff24ff756d8828f32ac76f27c
RishabhSharma7818 <rishabhsharma7818@gmail.com>
2026-06-24 13:11:40 +0530

    Create main application window

--- src/ui/QuizFrame.java
+++ src/ui/QuizFrame.java
@@ -1,5 +1,13 @@
 package ui;
 
-public class QuizFrame {
-    
+import javax.swing.JFrame;
+
+public class QuizFrame extends JFrame{
+    public QuizFrame(){
+        setTitle("Quiz Application");
+        setSize(700, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
