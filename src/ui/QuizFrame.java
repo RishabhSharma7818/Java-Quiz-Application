90ad4a3fe1de48349b015dfb865c44e84b5706fc
RishabhSharma7818 <rishabhsharma7818@gmail.com>
2026-06-24 13:40:33 +0530

    Add quiz screen layout

--- src/ui/QuizFrame.java
+++ src/ui/QuizFrame.java
@@ -1,13 +1,69 @@
 package ui;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class QuizFrame extends JFrame{
-    public QuizFrame(){
+public class QuizFrame extends JFrame {
+
+    private JLabel questionLabel;
+
+    private JRadioButton option1;
+    private JRadioButton option2;
+    private JRadioButton option3;
+    private JRadioButton option4;
+
+    private JButton nextButton;
+
+    public QuizFrame() {
+
+        initializeFrame();
+        initializeComponents();
+
+        setVisible(true);
+    }
+
+    private void initializeFrame() {
+
         setTitle("Quiz Application");
         setSize(700, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initializeComponents() {
+
+        setLayout(new BorderLayout());
+
+        questionLabel = new JLabel("What is JVM?");
+        questionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(questionLabel, BorderLayout.NORTH);
+
+        JPanel optionsPanel = new JPanel();
+
+        optionsPanel.setLayout(new GridLayout(4, 1));
+
+        option1 = new JRadioButton("Java Virtual Machine");
+        option2 = new JRadioButton("Java Variable Method");
+        option3 = new JRadioButton("Java Very Machine");
+        option4 = new JRadioButton("None");
+
+        ButtonGroup group = new ButtonGroup();
+
+        group.add(option1);
+        group.add(option2);
+        group.add(option3);
+        group.add(option4);
+
+        optionsPanel.add(option1);
+        optionsPanel.add(option2);
+        optionsPanel.add(option3);
+        optionsPanel.add(option4);
+
+        add(optionsPanel, BorderLayout.CENTER);
+
+        nextButton = new JButton("Next");
+
+        add(nextButton, BorderLayout.SOUTH);
     }
 }
